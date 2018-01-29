92c7a9240d669101262d9b2fff3084e6b55c76ca
sgbowles <sgbowles@vt.edu>
2018-01-29 11:51:04 -0500

    fixed config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = "vqqeyYCvaoUqDVT4pcWwQzYak"
-consumer_secret = "ZKHRJ7FGedpubFRRdzDRfd2ImoUZCJpzdzRZKjWF5vo8m7ds9B"
-access_key = "885836235012550657-5a1fnxSn9zgaPop5VAh15Nkjx4BpgiY"
-access_secret = "78tl8oIXt8b7jyzBnittHPxfKcF1OqLKs3E1QES5Kyzv5"
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
